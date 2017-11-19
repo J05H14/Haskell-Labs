4342d7a248e35364a645f8b1d8c3176c9df6fd83
J05H14 <j05h14@yahoo.com>
2017-11-19 15:24:48 -0800

    1-3 done

--- HW3.hs
+++ HW3.hs
@@ -4,7 +4,7 @@ data Student = Student{ firstName :: String
                       , major :: String
                       , age :: Int}deriving Show
 --2
-data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Read, Eq, Ord)
 
 makeNode :: Student  -> Tree Student
 makeNode x = Node x EmptyTree EmptyTree
@@ -12,6 +12,16 @@ makeNode x = Node x EmptyTree EmptyTree
 treeInsert :: Student -> Tree Student -> Tree Student
 treeInsert x EmptyTree = makeNode x
 treeInsert x (Node a left right)
-    | x { age } == a { age } = Node x left right
-    | x { age } < a { age } = Node a (treeInsert x left) right
-    | x { age } > a { age } = Node a left (treeInsert x right)
+    | getAge x == getAge a = Node x left right
+    | getAge x < getAge a = Node a (treeInsert x left) right
+    | getAge x > getAge a = Node a left (treeInsert x right)
+
+getAge :: Student -> Int
+getAge Student {age = a} = a
+
+--3 csv file
+
+--4
+
+main = do
+    
