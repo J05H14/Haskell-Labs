32926a65682d2653922152922e14ec57140b6a0a
J05H14 <j05h14@yahoo.com>
2017-11-21 23:11:11 -0800

    finished hw3

--- HW3/hhw3.hs
+++ HW3/hhw3.hs
@@ -30,11 +30,16 @@ getAge Student {age = a} = a
 
 --a
 main = do
-    fileName <- getArgs
-    contents <- readFile (head fileName)
+    args <- getArgs
+    contents <- readFile (head args)
+    
     let list = splitOn "\n" contents
+        searchType = head $ tail args
+        searchVal = head $ tail $ tail args
         stuList = parseCsv list
-    print stuList
+        stuTree = makeStudentTree stuList
+        answer = search (searchType) (searchVal) (stuTree)
+    print answer
 
 parseCsv :: [String] -> [Student]
 parseCsv [] = []
@@ -48,5 +53,44 @@ makeStudent :: [String] -> Student
 makeStudent [fn,ln,maj,age] = Student fn ln maj (read age)
 
 --b
+makeStudentTree :: [Student] -> Tree Student
 makeStudentTree [x] = treeInsert x EmptyTree
 makeStudentTree (x:xs) = treeInsert x (makeStudentTree xs)
+
+--c
+treeElem :: Int -> Tree Student -> Bool
+treeElem x EmptyTree = False
+treeElem x (Node a left right)
+    | x == getAge a = True
+    | x < getAge a = treeElem x left
+    | x > getAge a = treeElem x right
+
+searchByAge :: Int -> Tree Student -> Bool
+searchByAge age tree = treeElem age tree
+
+--d
+depthFirst :: Tree Student -> [Student]
+depthFirst EmptyTree = []
+depthFirst (Node a left right) = a : (depthFirst left) ++ (depthFirst right)
+
+searchByName :: String -> Tree Student -> Bool
+searchByName name tree =
+    let list = depthFirst tree
+    in findName name list
+
+findName :: String -> [Student] -> Bool
+findName name [] = False
+findName name (x:xs)
+    | getLastName x == name = True
+    | otherwise = findName name xs
+
+getLastName :: Student -> String
+getLastName Student {lastName = l} = l
+
+-- use c or d
+search :: [Char] -> String -> Tree Student -> Bool
+search "" "" EmptyTree = False
+search sType sVal tree
+    | sType == "search-age" = searchByAge value tree
+    | sType == "search-name" = searchByName sVal tree
+    where value = read sVal
